22c71f499baabe47cb654b9a5f416662835568ca
Alabhya Jindal <alabhya10@gmail.com>
2022-02-17 02:49:21 +0530

    init

---
+++ app.js
@@ -0,0 +1,28 @@
+const letters = "0123456789ABCDEF";
+const divs = document.querySelectorAll("div");
+
+console.log(divs);
+
+
+function updateColor() {
+
+  for (let j = 0; j < divs.length; j++) {
+    let color = ['#'];
+    let colorMC = letters[(Math.floor(Math.random() * 16))];
+
+
+    for (let i = 0; i < 6; i++) {
+      let colorAll = letters[(Math.floor(Math.random() * 16))]
+      color.push(colorAll);
+    }
+    color = color.join("");
+    console.log(color);
+    console.log(divs[j]);
+    
+    divs[j].style.backgroundColor = `${color}`;
+  }
+
+  
+}
+
+setInterval(updateColor, 300);
